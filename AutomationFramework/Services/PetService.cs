00c50875a3e733f7b0c1ec61db0b776604edce33
balaji-githubstore <balajidinakaran1@gmail.com>
2026-05-13 11:45:02 -0700

    Added services

---
+++ AutomationFramework/Services/PetService.cs
@@ -0,0 +1,22 @@
+﻿using AutomationFramework.Base;
+using RestSharp;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AutomationFramework.Services
+{
+    /// <summary>
+    /// All the resource for Pet will be available here
+    /// </summary>
+    public class PetService : BaseClient
+    {
+        public RestResponse GetPetById(int id)
+        {
+            var request = new RestRequest("pet/{petId}", Method.Get);
+            request.AddUrlSegment("petId", id);
+            return base.Execute(request);
+        }
+
+    }
+}
