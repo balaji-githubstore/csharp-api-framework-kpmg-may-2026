729e17f901e1ab568d21fa322c40ca4751f17a56
balaji-githubstore <balajidinakaran1@gmail.com>
2026-05-13 11:34:10 -0700

    Add project files.

---
+++ AutomationFramework/Base/BaseClient.cs
@@ -0,0 +1,23 @@
+﻿using RestSharp;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AutomationFramework.Base
+{
+    public class BaseClient
+    {
+        protected readonly RestClient client;
+
+        public BaseClient()
+        {
+            client = new RestClient(baseUrl: "https://petstore.swagger.io/v2");
+        }
+
+        protected RestResponse Execute(RestRequest request)
+        {
+            return client.Execute(request);
+        }
+       
+    }
+}
