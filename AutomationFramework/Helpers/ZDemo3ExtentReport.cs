6525b766c97c3a0982afc1f131a8a938d2284f19
balaji-githubstore <balajidinakaran1@gmail.com>
2026-05-14 12:34:28 -0700

    Updated Code

--- AutomationFramework/Helpers/ZDemo3ExtentReport.cs
+++ AutomationFramework/Helpers/ZDemo3ExtentReport.cs
@@ -1,35 +1,35 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-using AventStack.ExtentReports;
-using AventStack.ExtentReports.Reporter;
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Text;
+//using AventStack.ExtentReports;
+//using AventStack.ExtentReports.Reporter;
 
-namespace AutomationFramework.Helpers
-{
-    public class ZDemo3ExtentReport
-    {
-        [Test]
-        public void ExtentTest()
-        {
-            //run only once in the beginning for the application 
-            ExtentSparkReporter reporter = new ExtentSparkReporter("spark.html");
-            ExtentReports extent = new ExtentReports();
-            extent.AttachReporter(reporter);
+//namespace AutomationFramework.Helpers
+//{
+//    public class ZDemo3ExtentReport
+//    {
+//        [Test]
+//        public void ExtentTest()
+//        {
+//            //run only once in the beginning for the application 
+//            ExtentSparkReporter reporter = new ExtentSparkReporter("spark.html");
+//            ExtentReports extent = new ExtentReports();
+//            extent.AttachReporter(reporter);
 
-            //before the [Test] run
-            ExtentTest test= extent.CreateTest("AddPetTest");
-
-
-            test.Log(Status.Info, "Request server to add pet");
+//            //before the [Test] run
+//            ExtentTest test= extent.CreateTest("AddPetTest");
 
 
-            //After the [Test] run even-always
-            test.Log(Status.Pass, "AddPetTestPassed");
+//            test.Log(Status.Info, "Request server to add pet");
 
 
-            //flush to generate report // at the last after all [Test] run
-            extent.Flush();
+//            //After the [Test] run even-always
+//            test.Log(Status.Pass, "AddPetTestPassed");
 
-        }
-    }
-}
+
+//            //flush to generate report // at the last after all [Test] run
+//            extent.Flush();
+
+//        }
+//    }
+//}
