fcf27741c57d3df5df1741b688c254e280cb4852
balaji-githubstore <balajidinakaran1@gmail.com>
2026-05-15 01:55:18 -0700

    final session

---
+++ AutomationFramework/Helpers/ConfigurationHelper.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AutomationFramework.Helpers
+{
+    public static class ConfigurationHelper
+    {
+        private static IConfigurationRoot configuration;
+
+        static ConfigurationHelper()
+        {
+            configuration = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+                .Build();
+        }
+
+        public static string GetBaseUrl()
+        {
+            return configuration["ApiSettings:BaseUrl"];
+        }
+
+        public static int GetTimeout()
+        {
+            return int.Parse(configuration["ApiSettings:Timeout"]);
+        }
+    }
+}
